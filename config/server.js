2d0bff507705c86058a528a38484626b554d72ca
vinilopes91 <vinilopes13@hotmail.com>
2021-07-06 22:29:23 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2d6ba8291df0ef818bf73c2fb127697b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '251a16f9-975d-4465-8542-aed78d0fff32'
+};
